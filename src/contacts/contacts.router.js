7c1a059bbb7341dd18d4b66063312bbb3a43477c
Oleksandr Yashin <aveodenis@gmail.com>
2021-02-18 17:38:11 +0200

    add valid id

--- src/contacts/contacts.router.js
+++ src/contacts/contacts.router.js
@@ -6,9 +6,9 @@ import { contactValidation } from './../helpers/validate.js'
 const contactRouter = express.Router();
 
 contactRouter.get('/', listContacts);
-contactRouter.get('/:contactId', getContactById);
+contactRouter.get('/:contactId', contactValidation(), getContactById);
 contactRouter.post('/', contactValidation(addContactSchema), addContact);
-contactRouter.delete('/:contactId', removeContact);
+contactRouter.delete('/:contactId', contactValidation(), removeContact);
 contactRouter.patch('/:contactId', contactValidation(updateContactSchema), updateContact);
 
 export default contactRouter;
