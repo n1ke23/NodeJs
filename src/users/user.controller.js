83b1fed2da3ab2112ee6c8836280721b0542d8a5
Oleksandr Yashin <aveodenis@gmail.com>
2021-02-25 06:25:17 +0200

    add code

--- src/users/user.controller.js
+++ src/users/user.controller.js
@@ -1,4 +1,38 @@
 import { userModel } from "./user.model.js";
+import bcrypt from 'bcrypt';
+
+
+export async function updateUser(req, res, next) {
+    try {
+        const { _id, avatarURL, email: oldEmail, password: oldPasword } = req.user;
+        const { filename } = req.file;
+        const { email, password, avatarURL: newAvatar } = req.body
+        const existUser = await userModel.findOne({ email });
+        if (existUser) {
+            return res.status(409).json({ message: "Email in use" });
+        }
+        if (newAvatar) {
+            const oldAvatar = avatarURL.replace(baseAvatarURL, '');
+            const src = path.join(__dirname, (`../../public/images/${oldAvatar}`));
+            await fs.promises.unlink(src);
+        }
+
+        const newAvatarURL = baseAvatarURL + filename;
+        const passwordHash = await bcrypt.hash(password, Number(process.env.SALT_ROUNDS));
+        const baseAvatarURL = 'http://localhost:3000/images/';
+        const updatedUser = await UserModel.findByIdAndUpdate(_id, {
+            avatarURL: newAvatar ? newAvatarURL : avatarURL,
+            email: email ? email : oldEmail,
+            password: password ? passwordHash : oldPasword
+        }, { new: true });
+
+
+        return res.status(200).send({ avatarURL: updatedUser.avatarURL, email, password: passwordHash });
+    } catch (error) {
+        next(error);
+    }
+}
+
 
 export async function updateSubscription(req, res, next) {
     try {
